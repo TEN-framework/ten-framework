a5c9eded2e5bfcbc97625219ce643df9b0f2b740
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-05-21 19:22:04 +0800

    Update signal.c

--- core/src/ten_runtime/global/signal.c
+++ core/src/ten_runtime/global/signal.c
@@ -121,6 +121,7 @@ static void ten_global_sigsegv_handler(TEN_UNUSED int signo, siginfo_t *info,
   // Wait for a short period to allow backtrace to be written.
   ten_sleep_ms(200);
 
+  // Using `abort()` instead of `_exit()` allows a core dump to be generated.
   abort();
 }
 
