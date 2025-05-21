71745a1a811b9b355a32ee4449dcf290c77dee5a
czhen <56986964+shczhen@users.noreply.github.com>
2025-05-21 16:03:40 +0800

    fix: graph table

--- core/src/ten_manager/designer_frontend/src/components/Popup/Default/GraphSelect.tsx
+++ core/src/ten_manager/designer_frontend/src/components/Popup/Default/GraphSelect.tsx
@@ -7,7 +7,12 @@
 import * as React from "react";
 import { useTranslation } from "react-i18next";
 import { toast } from "sonner";
-import { CheckIcon } from "lucide-react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 
 import {
   Select,
@@ -18,6 +23,16 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/Select";
+import { Checkbox } from "@/components/ui/Checkbox";
+import { Badge } from "@/components/ui/Badge";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/Table";
 import { Label } from "@/components/ui/Label";
 import { Button } from "@/components/ui/Button";
 import { SpinnerLoading } from "@/components/Status/Loading";
@@ -52,18 +67,14 @@ export const GraphSelectPopupContent = (props: { widget: IWidget }) => {
   const [selectedApp, setSelectedApp] = React.useState<IApp | null>(
     currentWorkspace?.app ?? loadedApps?.app_info?.[0] ?? null
   );
-  const [tmpSelectedGraph, setTmpSelectedGraph] = React.useState<IGraph | null>(
-    currentWorkspace.graph ?? null
-  );
 
   const { graphs = [], error, isLoading } = useGraphs();
 
-  const handleSelectApp = (app?: IApp | null) => {
-    setSelectedApp(app ?? null);
-    setTmpSelectedGraph(null);
+  const handleOk = () => {
+    removeWidget(widget.widget_id);
   };
 
-  const handleSelectGraph = (graph: IGraph) => async () => {
+  const handleSelectGraph = async (graph: IGraph) => {
     updateCurrentWorkspace({
       app: selectedApp,
       graph,
@@ -73,46 +84,21 @@ export const GraphSelectPopupContent = (props: { widget: IWidget }) => {
         await resetNodesAndEdgesByGraph(graph);
 
       setNodesAndEdges(layoutedNodes, layoutedEdges);
+
+      toast.success(t("popup.selectGraph.updateSuccess"), {
+        description: (
+          <>
+            <p>{`${t("popup.selectGraph.app")}: ${selectedApp?.base_dir}`}</p>
+            <p>{`${t("popup.selectGraph.graph")}: ${graph.name}`}</p>
+          </>
+        ),
+      });
     } catch (err: unknown) {
       console.error(err);
       toast.error("Failed to load graph.");
     } finally {
-      removeWidget(widget.widget_id);
+      // removeWidget(widget.widget_id);
     }
-  };
-
-  const handleCancel = () => {
-    removeWidget(widget.widget_id);
-    handleSelectApp(currentWorkspace.app);
-    setTmpSelectedGraph(currentWorkspace.graph);
-  };
-
-  const handleSave = async () => {
-    if (!tmpSelectedGraph) {
-      updateCurrentWorkspace({
-        app: selectedApp,
-        graph: null,
-      });
-      setNodesAndEdges([], []);
-      toast.success(t("popup.selectGraph.updateSuccess"), {
-        description: (
-          <>
-            <p>{`${t("popup.selectGraph.app")}: ${selectedApp?.base_dir}`}</p>
-          </>
-        ),
-      });
-    } else {
-      await handleSelectGraph(tmpSelectedGraph)();
-      toast.success(t("popup.selectGraph.updateSuccess"), {
-        description: (
-          <>
-            <p>{`${t("popup.selectGraph.app")}: ${selectedApp?.base_dir}`}</p>
-            <p>{`${t("popup.selectGraph.graph")}: ${tmpSelectedGraph.name}`}</p>
-          </>
-        ),
-      });
-    }
-    removeWidget(widget.widget_id);
   };
 
   React.useEffect(() => {
@@ -172,60 +158,149 @@ export const GraphSelectPopupContent = (props: { widget: IWidget }) => {
           />
         </>
       ) : (
-        <ul className="flex flex-col gap-1 h-fit overflow-y-auto">
-          <Button
-            asChild
-            key={"null"}
-            className="justify-start cursor-pointer"
-            variant="ghost"
-            onClick={() => setTmpSelectedGraph(null)}
-          >
-            <li className="w-full">
-              {tmpSelectedGraph === null ? (
-                <CheckIcon className="size-4" />
-              ) : (
-                <div className="size-4" />
-              )}
-              <span className="text-sm">
-                {t("popup.selectGraph.unspecified")}
-              </span>
-            </li>
-          </Button>
-          {graphs
-            ?.filter((graph) => graph.base_dir === selectedApp?.base_dir)
-            ?.map((graph) => (
-              <Button
-                asChild
-                key={graph.name}
-                className="justify-start cursor-pointer"
-                variant="ghost"
-                onClick={() => setTmpSelectedGraph(graph)}
-              >
-                <li className="w-full">
-                  {tmpSelectedGraph?.uuid === graph.uuid ? (
-                    <CheckIcon className="size-4" />
-                  ) : (
-                    <div className="size-4" />
-                  )}
-                  <span className="text-sm">{graph.name}</span>
-                  {graph.auto_start ? (
-                    <span className="text-xs">({t("action.autoStart")})</span>
-                  ) : null}
-                  {selectedApp?.base_dir === currentWorkspace.app?.base_dir &&
-                  graph.name === currentWorkspace.graph?.name ? (
-                    <span className="text-xs">({t("action.current")})</span>
-                  ) : null}
-                </li>
-              </Button>
-            ))}
-        </ul>
+        <GraphSelectTable
+          items={graphs?.filter(
+            (graph) => graph.base_dir === selectedApp?.base_dir
+          )}
+          onSelect={handleSelectGraph}
+        />
       )}
-      <div className="flex justify-end gap-2">
-        <Button variant="outline" onClick={handleCancel}>
-          {t("action.cancel")}
+      <div className="flex mt-auto justify-end gap-2">
+        <Button variant="default" onClick={handleOk}>
+          {t("action.ok")}
         </Button>
-        <Button onClick={handleSave}>{t("action.save")}</Button>
       </div>
     </div>
+  );
+};
+
+const GraphSelectTable = (props: {
+  items?: IGraph[];
+  onSelect?: (item: IGraph) => void;
+}) => {
+  const { items = [], onSelect } = props;
+
+  const { t } = useTranslation();
+
+  const columns: ColumnDef<
+    Partial<IGraph> & {
+      isCurrent?: boolean;
+    }
+  >[] = [
+    {
+      accessorKey: "name",
+      header: "Name",
+      cell: ({ row, getValue }) => {
+        const value = getValue() as string;
+        const isCurrent = row.getValue("isCurrent") as boolean;
+        return (
+          <div className="flex items-center">
+            <span className="text-sm">{value}</span>
+            {isCurrent && (
+              <Badge className="ml-2" variant="outline">
+                {t("action.current")}
+              </Badge>
+            )}
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "auto_start",
+      header: "Auto Start",
+      cell: ({ getValue }) => {
+        const value = getValue() as boolean;
+        return (
+          <div className="flex items-center">
+            <Checkbox disabled checked={value} />
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "action",
+      header: "Action",
+      cell: ({ row }) => {
+        const isCurrent = row.getValue("isCurrent") as boolean;
+        return (
+          <div className="flex items-center">
+            <Button
+              size="sm"
+              variant="outline"
+              disabled={isCurrent}
+              onClick={() => {
+                const graph = row.original as IGraph;
+                onSelect?.(graph);
+              }}
+            >
+              {isCurrent
+                ? t("popup.selectGraph.selected")
+                : t("popup.selectGraph.select")}
+            </Button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  const table = useReactTable<Partial<IGraph> & { isCurrent?: boolean }>({
+    data: items,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </>
   );
 };
