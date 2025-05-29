96c42a39e2502d884221b0ffab43d16b17c7c6e2
sunxilin <sunxilin@shengwang.cn>
2025-05-30 00:01:03 +0800

    fix: test cases

---
+++ tests/ten_runtime/integration/python/unused_addon_python/unused_addon_python_app/main.go
@@ -0,0 +1,54 @@
+//
+// This file is part of TEN Framework, an open source project.
+// Licensed under the Apache License, Version 2.0.
+// See the LICENSE file for more information.
+//
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"runtime/debug"
+	"time"
+
+	ten "ten_framework/ten_runtime"
+)
+
+type defaultApp struct {
+	ten.DefaultApp
+}
+
+func (p *defaultApp) OnInit(tenEnv ten.TenEnv) {
+	tenEnv.LogDebug("onInit")
+	tenEnv.OnInitDone()
+}
+
+func (p *defaultApp) OnDeinit(tenEnv ten.TenEnv) {
+	tenEnv.LogDebug("onDeinit")
+	tenEnv.OnDeinitDone()
+}
+
+func main() {
+	// test app
+	app, err := ten.NewApp(&defaultApp{})
+	if err != nil {
+		fmt.Println("Failed to create app.")
+	}
+
+	app.Run(true)
+	app.Wait()
+
+	// A single GC is not enough; multiple rounds of GC are needed to clean up
+	// as thoroughly as possible.
+	for i := 0; i < 10; i++ {
+		// Explicitly trigger GC to increase the likelihood of finalizer
+		// execution.
+		debug.FreeOSMemory()
+		runtime.GC()
+
+		// Wait for a short period to give the GC time to run.
+		runtime.Gosched()
+		time.Sleep(1 * time.Second)
+	}
+}
