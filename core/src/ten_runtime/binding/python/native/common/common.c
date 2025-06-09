6f98687599fff12ae00b98412e49637cba60ea1d
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-06-09 20:57:53 +0800

    fix: refine codes

--- core/src/ten_runtime/binding/python/native/common/common.c
+++ core/src/ten_runtime/binding/python/native/common/common.c
@@ -75,18 +75,14 @@ void ten_py_initialize_with_config(const char *program,
 }
 
 int ten_py_finalize(void) {
-  Py_ssize_t collected = 0;
-  int rounds = 0;
-  const int max_rounds = 10;
-
   // Multiple rounds of collection until there are no more objects to collect or
   // the maximum number of rounds is reached.
-  do {
-    collected = PyGC_Collect();
-    rounds++;
-  } while (collected > 0 && rounds < max_rounds);
-
-  return Py_FinalizeEx();
+  for (int i = 0; i < 10; i++) {
+    Py_ssize_t collected = PyGC_Collect();
+    if (collected == 0) {
+      break;  // No more objects to collect.
+    }
+  }
 
   return Py_FinalizeEx();
 }
