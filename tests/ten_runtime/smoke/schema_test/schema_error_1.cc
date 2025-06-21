3d653e0a1f7248b938c71805e792ba0b673cf9bc
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-06-21 22:33:40 +0800

    fix: refine codes

--- tests/ten_runtime/smoke/schema_test/schema_error_1.cc
+++ tests/ten_runtime/smoke/schema_test/schema_error_1.cc
@@ -33,8 +33,10 @@ class test_extension : public ten::extension_t {
              "version": "0.1.0",
              "api": {
                "property": {
+               "properties": {
                  "hello": {
                    "type": "string"
+                 }
                  }
                }
              }
