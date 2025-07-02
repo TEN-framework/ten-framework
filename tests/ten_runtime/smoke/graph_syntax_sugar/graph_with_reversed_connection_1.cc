e5604910b05a9886d468af8c5930705d5e27aebd
sunxilin <sunxilin@shengwang.cn>
2025-07-02 19:04:19 +0800

    fix: test case

--- tests/ten_runtime/smoke/graph_syntax_sugar/graph_with_reversed_connection_1.cc
+++ tests/ten_runtime/smoke/graph_syntax_sugar/graph_with_reversed_connection_1.cc
@@ -78,7 +78,7 @@ TEN_CPP_REGISTER_ADDON_AS_EXTENSION(
 
 }  // namespace
 
-TEST(ExtensionTest, GraphWithReversedConnection1) {  // NOLINT
+TEST(GraphSyntaxSugarTest, GraphWithReversedConnection1) {  // NOLINT
   // Start app.
   auto *app_thread =
       ten_thread_create("app thread", test_app_thread_main, nullptr);
--- tests/ten_runtime/smoke/graph_syntax_sugar/graph_with_reversed_connection_2.cc
+++ tests/ten_runtime/smoke/graph_syntax_sugar/graph_with_reversed_connection_2.cc
@@ -107,7 +107,7 @@ TEN_CPP_REGISTER_ADDON_AS_EXTENSION(
 
 }  // namespace
 
-TEST(ExtensionTest, GraphWithReversedConnection2) {  // NOLINT
+TEST(GraphSyntaxSugarTest, GraphWithReversedConnection2) {  // NOLINT
   // Start app.
   auto *app_thread =
       ten_thread_create("app thread", test_app_thread_main, nullptr);
