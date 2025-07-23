9a94ac34e339ccdb525450242af5bc5c157fe44b
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-07-23 19:37:19 +0800

    fix: refine codes

--- core/include/ten_utils/lang/cpp/lib/value_buffer.h
+++ core/include/ten_utils/lang/cpp/lib/value_buffer.h
@@ -13,54 +13,21 @@
 
 namespace ten {
 
-namespace value_buffer {
-
 inline uint8_t *serialize_to_buffer(const value_t &value, size_t *buffer_size,
                                     error_t *err = nullptr) {
-  // Placeholder implementation - would use C layer buffer protocol
-  // when available
-  (void)value;
-  if (buffer_size) *buffer_size = 0;
-  if (err) {
-    err->set_error_code(TEN_ERROR_CODE_GENERIC);
-  }
+  // TODO(Wei): use C layer buffer protocol
+
   return nullptr;
 }
 
-/**
- * @brief Placeholder for deserialize_from_buffer function.
- *
- * This function would deserialize a value_t from a buffer created by
- * serialize_to_buffer. Currently returns an invalid value as this is
- * a placeholder implementation.
- *
- * When the C layer buffer protocol functions are available, this would be
- * implemented as a simple wrapper around those functions.
- *
- * @param buffer Pointer to buffer data (currently unused)
- * @param buffer_size Size of buffer (currently unused)
- * @param bytes_consumed Output parameter (currently unused)
- * @param err Optional error object (currently unused)
- * @return An invalid value_t (placeholder implementation)
- */
 inline value_t deserialize_from_buffer(const uint8_t *buffer,
                                        size_t buffer_size,
                                        size_t *bytes_consumed = nullptr,
                                        error_t *err = nullptr) {
-  // Placeholder implementation - would use C layer buffer protocol
-  // when available
-  (void)buffer;
-  (void)buffer_size;
-  if (bytes_consumed) *bytes_consumed = 0;
-  if (err) {
-    err->set_error_code(TEN_ERROR_CODE_GENERIC);
-  }
+  // TODO(Wei): use C layer buffer protocol
 
   // Return an invalid value as placeholder
-  // In real implementation, this would deserialize from buffer
-  // For now, just return a default constructed value_t
   return value_t();
 }
 
-}  // namespace value_buffer
 }  // namespace ten
