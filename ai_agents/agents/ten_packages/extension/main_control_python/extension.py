e82fd527d565d8d242ec4b881179a9de2a9a39ab
plutoless <zhangqianze@agora.io>
2025-07-28 14:44:31 +0800

    fix: refactor code

--- ai_agents/agents/ten_packages/extension/main_control_python/extension.py
+++ ai_agents/agents/ten_packages/extension/main_control_python/extension.py
@@ -4,11 +4,8 @@
 # See the LICENSE file for more information.
 #
 import asyncio
-from enum import Enum
 import json
-import time
-from typing import Any, Awaitable, Callable
-import uuid
+from typing import Any, Awaitable, Optional, Tuple
 from pydantic import BaseModel
 from ten_runtime import (
     AsyncExtension,
@@ -17,6 +14,7 @@ from ten_runtime import (
     StatusCode,
     CmdResult,
     Data,
+    TenError,
 )
 
 class MainControlConfig(BaseModel):
@@ -27,55 +25,76 @@ class MainControlExtension(AsyncExtension):
         super().__init__(name)
         self._rtc_user_count = 0
         self.config = None
+        self.cmd_events = asyncio.Queue[Tuple[str, Cmd]]()
+        self.data_events = asyncio.Queue[Tuple[str, Data]]()
+        self.ten_env: AsyncTenEnv = None
+        self.stopped = False
 
     async def on_init(self, ten_env: AsyncTenEnv) -> None:
+        self.ten_env = ten_env
         config_json, _ = await ten_env.get_property_to_json(None)
         self.config = MainControlConfig.model_validate_json(config_json)
         ten_env.log_debug("on_init")
+
+        asyncio.create_task(self._process_cmd_events())
+        asyncio.create_task(self._process_data_events())
 
     async def on_start(self, ten_env: AsyncTenEnv) -> None:
         ten_env.log_debug("on_start")
 
     async def on_stop(self, ten_env: AsyncTenEnv) -> None:
         ten_env.log_debug("on_stop")
-
-    async def on_deinit(self, ten_env: AsyncTenEnv) -> None:
-        ten_env.log_debug("on_deinit")
+        self.stopped = True
 
     async def on_cmd(self, ten_env: AsyncTenEnv, cmd: Cmd) -> None:
         cmd_name = cmd.get_name()
         ten_env.log_info(f"on_cmd name {cmd_name}")
 
-        if cmd_name == "on_user_joined":
-            await self._on_cmd_on_user_joined(ten_env, cmd)
-        elif cmd_name == "on_user_left":
-            await self._on_cmd_on_user_left(ten_env, cmd)
+        await self.cmd_events.put([cmd_name, cmd])
 
         cmd_result = CmdResult.create(StatusCode.OK, cmd)
         await ten_env.return_result(cmd_result)
 
-    async def _on_cmd_on_user_joined(
-        self, ten_env: AsyncTenEnv, cmd: Cmd
-    ) -> None:
-        assert self.config is not None
-        self._rtc_user_count += 1
-        if self._rtc_user_count == 1 and self.config.greeting:
-            await self._request_tts(ten_env, self.config.greeting)
-            await self._send_caption(ten_env, self.config.greeting, True, True, 100)
-
-    async def _on_cmd_on_user_left(
-        self, ten_env: AsyncTenEnv, cmd: Cmd
-    ) -> None:
-        self._rtc_user_count -= 1
 
     async def on_data(self, ten_env: AsyncTenEnv, data: Data) -> None:
         data_name = data.get_name()
         ten_env.log_info(f"on_data name {data_name}")
 
-        if data_name == "asr_result":
-            await self._on_data_asr_result(ten_env, data)
-        elif data_name == "llm_result":
-            await self._on_data_llm_result(ten_env, data)
+        await self.data_events.put([data_name, data])
+
+    async def _process_data_events(self) -> None:
+        while self.stopped is False:
+            try:
+                data_name, data = await self.data_events.get()
+                match data_name:
+                    case "asr_result":
+                        await self._on_data_asr_result(self.ten_env, data)
+                    case "llm_result":
+                        await self._on_data_llm_result(self.ten_env, data)
+                    case _:
+                        self.ten_env.log_info(f"Unknown data: {data_name}")
+            except Exception as e:
+                self.ten_env.log_error(f"Error processing data: {e}")
+
+    async def _process_cmd_events(self) -> None:
+        while self.stopped is False:
+            try:
+                cmd_name, cmd = await self.cmd_events.get()
+
+                match cmd_name:
+                    case "on_user_joined":
+                        self._rtc_user_count += 1
+                        if self._rtc_user_count == 1 and self.config.greeting:
+                            await self._send_to_tts(self.ten_env, self.config.greeting)
+                            await self._send_transcript(self.ten_env, self.config.greeting, True, True, 100)
+                    case "on_user_left":
+                        self._rtc_user_count -= 1
+                    case _:
+                        self.ten_env.log_info(f"Unknown command: {cmd_name}")
+            except Exception as e:
+                self.ten_env.log_error(f"Error processing command: {e}")
+
+
 
     async def _on_data_asr_result(
         self, ten_env: AsyncTenEnv, data: Data
@@ -87,11 +106,11 @@ class MainControlExtension(AsyncExtension):
         metadata = asr_result_dict.get("metadata", {})
         stream_id = int(metadata.get("session_id", "100"))
         if final or len(text) > 2:
-            await self._flush(ten_env)
+            await self._interrupt(ten_env)
         if final:
-            await self._request_llm(ten_env, text, True)
+            await self._send_to_llm(ten_env, text, True)
 
-        await self._send_caption(ten_env, text, final, final, stream_id)
+        await self._send_transcript(ten_env, text, final, final, stream_id)
 
     async def _on_data_llm_result(
         self, ten_env: AsyncTenEnv, data: Data
@@ -100,46 +119,51 @@ class MainControlExtension(AsyncExtension):
         llm_result_dict = json.loads(llm_result_json)
         text = llm_result_dict.get("text", "")
         end_of_segment = llm_result_dict.get("end_of_segment", False)
-        await self._request_tts(ten_env, text)
-        await self._send_caption(ten_env, text, end_of_segment, True, 100)
+        await self._send_to_tts(ten_env, text)
+        await self._send_transcript(ten_env, text, end_of_segment, True, 100)
 
-    async def _request_tts(self, ten_env: AsyncTenEnv, text: str):
-        q = Data.create("text_data")
-        q.set_dest(None, None, "tts")
-        q.set_property_string("text", text)
-        await ten_env.send_data(q)
-        ten_env.log_info(f"request_tts: text {text}")
+    async def _send_to_tts(self, ten_env: AsyncTenEnv, text: str):
+        await self._send_data(ten_env, "text_data", "tts", {"text": text})
+        ten_env.log_info(f"_send_to_tts: text {text}")
 
-    async def _request_llm(self, ten_env: AsyncTenEnv, text: str, is_final: bool):
-        q = Data.create("text_data")
-        q.set_dest(None, None, "llm")
-        q.set_property_string("text", text)
-        q.set_property_bool("is_final", is_final)
-        await ten_env.send_data(q)
-        ten_env.log_info(f"request_llm: text {text}, is_final {is_final}")
+    async def _send_to_llm(self, ten_env: AsyncTenEnv, text: str, is_final: bool):
+        await self._send_data(ten_env, "text_data", "llm", {"text": text, "is_final": is_final})
+        ten_env.log_info(f"_send_to_llm: text {text}, is_final {is_final}")
 
-    async def _send_caption(self, ten_env: AsyncTenEnv, text: str, end_of_segment: bool, final: bool, stream_id: int):
-        caption = Data.create("text_data")
-        caption.set_dest(None, None, "message_collector")
-        caption.set_property_string("text", text)
-        caption.set_property_bool("is_final", final)
-        caption.set_property_int("stream_id", stream_id)
-        caption.set_property_bool("end_of_segment", end_of_segment)
-        await ten_env.send_data(caption)
-        ten_env.log_info(f"caption: text {text}, is_final {final}, end_of_segment {end_of_segment}, stream_id {stream_id}")
+    async def _send_transcript(self, ten_env: AsyncTenEnv, text: str, end_of_segment: bool, final: bool, stream_id: int):
+        await self._send_data(ten_env, "text_data", "message_collector", {"text": text, "is_final": final, "stream_id": stream_id, "end_of_segment": end_of_segment})
+        ten_env.log_info(f"_send_transcript: text {text}, is_final {final}, end_of_segment {end_of_segment}, stream_id {stream_id}")
 
-    async def _flush(self, ten_env: AsyncTenEnv):
-        flush_llm = Cmd.create("flush")
-        flush_llm.set_dest(None, None, "llm")
-        await ten_env.send_cmd(flush_llm)
-        ten_env.log_info("flush_llm")
+    async def _interrupt(self, ten_env: AsyncTenEnv):
+        await self._send_cmd(ten_env, "flush", "llm")
+        await self._send_cmd(ten_env, "flush", "tts")
+        await self._send_cmd(ten_env, "flush", "rtc")
 
-        flush_tts = Cmd.create("flush")
-        flush_tts.set_dest(None, None, "tts")
-        await ten_env.send_cmd(flush_tts)
-        ten_env.log_info("flush_tts")
 
-        flush_rtc = Cmd.create("flush")
-        flush_rtc.set_dest(None, None, "rtc")
-        await ten_env.send_cmd(flush_rtc)
-        ten_env.log_info("flush_rtc")
+    async def _send_cmd(
+        self, ten_env: AsyncTenEnv, cmd_name: str, dest: str, payload: Any = None
+    ) -> tuple[Optional[CmdResult], Optional[TenError]]:
+        """Convenient method to send a command with a payload within app/graph w/o need to create a connection."""
+        """Note: extension using this approach will contain logics that are meaningful for this graph only,"""
+        """as it will assume the target extension already exists in the graph."""
+        """For generate purpose extension, it should try to prevent using this method."""
+        cmd = Cmd.create(cmd_name)
+        cmd.set_dest(None, None, dest)
+        if payload is not None:
+            cmd.set_property_from_json(None, json.dumps(payload))
+        ten_env.log_debug(f"send_cmd: cmd_name {cmd_name}, dest {dest}")
+        return await ten_env.send_cmd(cmd)
+
+    async def _send_data(
+        self, ten_env: AsyncTenEnv, data_name: str, dest: str, payload: Any = None
+    ) -> Optional[TenError]:
+        """Convenient method to send data with a payload within app/graph w/o need to create a connection."""
+        """Note: extension using this approach will contain logics that are meaningful for this graph only,"""
+        """as it will assume the target extension already exists in the graph."""
+        """For generate purpose extension, it should try to prevent using this method."""
+        data = Data.create(data_name)
+        data.set_dest(None, None, dest)
+        if payload is not None:
+            data.set_property_from_json(None, json.dumps(payload))
+        ten_env.log_debug(f"send_data: data_name {data_name}, dest {dest}")
+        return await ten_env.send_data(data)
