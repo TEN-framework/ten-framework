527ad47e2a5ce43b4d6d5fe7a79869dea890c650
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-08-08 15:56:45 +0800

    fix: refine codes

--- build/ten_common/rust/cargo_build.py
+++ build/ten_common/rust/cargo_build.py
@@ -91,7 +91,6 @@ if __name__ == "__main__":
         cmd = [
             "cargo",
             "build",
-            "-vv",
             "--target-dir",
             args.target_path,
             "--target",
