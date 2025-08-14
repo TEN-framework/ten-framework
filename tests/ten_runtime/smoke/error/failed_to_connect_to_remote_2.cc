3336196eb51651e8cf18f6b8073df3ecb2d61af6
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-08-14 20:41:54 +0800

    fix: refine codes

--- tests/ten_runtime/smoke/error/failed_to_connect_to_remote_2.cc
+++ tests/ten_runtime/smoke/error/failed_to_connect_to_remote_2.cc
@@ -29,8 +29,8 @@ class test_predefined_graph : public ten::extension_t {
           auto status_code = cmd_result->get_status_code();
           ASSERT_EQ(status_code, TEN_STATUS_CODE_ERROR);
 
-          auto graph_id = cmd_result->get_property_string("graph_id");
-          ASSERT_EQ(graph_id, "Failed to connect to msgpack://127.0.0.1:8888/");
+          auto detail = cmd_result->get_property_string("detail");
+          ASSERT_EQ(detail, "Failed to connect to msgpack://127.0.0.1:8888/");
 
           // The app will not be closed because it is running in
           // long_running_mode.
