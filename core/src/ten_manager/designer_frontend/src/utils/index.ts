ef32dc066f04fc098347efb28a5e7b443d660ce5
czhen <56986964+shczhen@users.noreply.github.com>
2025-08-21 16:46:46 +0800

    fix: rm legacy func

--- core/src/ten_manager/designer_frontend/src/utils/index.ts
+++ core/src/ten_manager/designer_frontend/src/utils/index.ts
@@ -6,35 +6,25 @@
 //
 import { z } from "zod";
 
-export function getZodDefaults<Schema extends z.AnyZodObject>(schema: Schema) {
-  return Object.fromEntries(
-    Object.entries(schema.shape).map(([key, value]) => {
-      if (value instanceof z.ZodDefault)
-        return [key, value._def.defaultValue()];
-      return [key, undefined];
-    })
-  );
-}
-
 // example usage:
 // stringToJSONSchema.pipe(
 //   z.object({ id: z.string(), redirect_uri: z.string() })
 // )
 export const stringToJSONSchema = z.string().transform((str, ctx) => {
-  try {
-    return JSON.parse(str);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (e) {
-    ctx.addIssue({ code: "custom", message: "Invalid JSON" });
-    return z.NEVER;
-  }
+	try {
+		return JSON.parse(str);
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	} catch (e) {
+		ctx.addIssue({ code: "custom", message: "Invalid JSON" });
+		return z.NEVER;
+	}
 });
 
 export const getCurrentWindowSize = () => {
-  if (typeof window !== "undefined") {
-    return {
-      width: window.innerWidth,
-      height: window.innerHeight,
-    };
-  }
+	if (typeof window !== "undefined") {
+		return {
+			width: window.innerWidth,
+			height: window.innerHeight,
+		};
+	}
 };
