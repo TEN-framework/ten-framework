0cfc6aefb0357a28f3839aa6013866371a59d8d9
plutoless <qianze.zhang@hotmail.com>
2025-08-28 01:44:48 +0800

    feat: finalize mllm

--- ai_agents/agents/ten_packages/extension/default_mllm_extension_python/addon.py
+++ ai_agents/agents/ten_packages/extension/default_mllm_extension_python/addon.py
@@ -1,3 +1,8 @@
+#
+# This file is part of TEN Framework, an open source project.
+# Licensed under the Apache License, Version 2.0.
+# See the LICENSE file for more information.
+#
 from ten_runtime import (
     Addon,
     register_addon_as_extension,
@@ -5,10 +10,10 @@ from ten_runtime import (
 )
 
 
-@register_addon_as_extension("default_asr_extension_python")
-class DefaultASRExtensionAddon(Addon):
+@register_addon_as_extension("default_mllm_extension_python")
+class DefaultMLLMExtensionAddon(Addon):
     def on_create_instance(self, ten: TenEnv, addon_name: str, context) -> None:
-        from .extension import DefaultASRExtension
+        from .extension import DefaultMLLMExtension
 
         ten.log_info("on_create_instance")
-        ten.on_create_instance_done(DefaultASRExtension(addon_name), context)
+        ten.on_create_instance_done(DefaultMLLMExtension(addon_name), context)
