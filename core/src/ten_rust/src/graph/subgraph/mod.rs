5caee0cb06dcbf21a44bb8573348e49925930882
FaithfulinLOVE <905649683@qq.com>
2025-10-10 15:14:38 +0800

    test: add connection between selectors

--- core/src/ten_rust/src/graph/subgraph/mod.rs
+++ core/src/ten_rust/src/graph/subgraph/mod.rs
@@ -102,7 +102,7 @@ impl Graph {
                 Ok(extension_node.addon)
             }
             GraphNodeType::Selector => Err(anyhow::anyhow!(
-                "Selector nodes are not yet supported in get_addon_name_of_node"
+                "Selector node are not supported in get_addon_name_of_node, it should be broken down into extension nodes."
             )),
         }
     }
