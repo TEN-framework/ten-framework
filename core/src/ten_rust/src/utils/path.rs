9ea16d7e7bdef32dbd1f4e6eea34cbce7318fd2e
Nie Zhihe <niezhihe@shengwang.cn>
2025-10-11 17:47:35 +0800

    fix: ? prefix on windows

--- core/src/ten_rust/src/utils/path.rs
+++ core/src/ten_rust/src/utils/path.rs
@@ -36,6 +36,21 @@ pub fn normalize_path(path: &Path) -> PathBuf {
     ret
 }
 
+/// Sanitize the local path to make it a valid Windows path.
+/// It will strip the Windows verbatim prefix (e.g., \\?\ or \\?\UNC\) and
+/// replace the '/' with '\'.
+fn sanitize_windows_local_path(raw_path: Option<&str>) -> Option<String> {
+    raw_path.map(|path| {
+        if let Some(rest) = path.strip_prefix("\\\\?\\UNC\\") {
+            format!("\\\\{}", rest).replace('/', "\\")
+        } else if let Some(rest) = path.strip_prefix("\\\\?\\") {
+            rest.replace('/', "\\")
+        } else {
+            path.replace('/', "\\")
+        }
+    })
+}
+
 pub fn get_base_dir_of_uri(uri: &str) -> Result<String> {
     if let Ok(url) = Url::parse(uri) {
         match url.scheme() {
@@ -91,8 +106,8 @@ pub fn get_base_dir_of_uri(uri: &str) -> Result<String> {
 /// app_base_dir parameter.
 pub fn get_real_path_from_import_uri(
     import_uri: &str,
-    base_dir: Option<&str>,
-    app_base_dir: Option<&str>,
+    raw_base_dir: Option<&str>,
+    raw_app_base_dir: Option<&str>,
 ) -> Result<String> {
     // If the import_uri is an absolute path (without variable substitution),
     // return an error because absolute paths should use file:// URI
@@ -103,6 +118,12 @@ pub fn get_real_path_from_import_uri(
             import_uri
         ));
     }
+
+    // Windows only: Sanitize the base_dir and app_base_dir.
+    let base_dir_string = sanitize_windows_local_path(raw_base_dir);
+    let base_dir = base_dir_string.as_deref();
+    let app_base_dir_string = sanitize_windows_local_path(raw_app_base_dir);
+    let app_base_dir = app_base_dir_string.as_deref();
 
     // Check if import_uri contains ${app_base_dir} variable
     let processed_import_uri = if import_uri.contains("${app_base_dir}") {
