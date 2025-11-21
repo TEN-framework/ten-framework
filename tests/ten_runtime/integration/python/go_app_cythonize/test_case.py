dc509300b6368b9236c250911ee287602c69999c
Nie Zhihe <niezhihe@shengwang.cn>
2025-11-21 18:28:36 +0800

    fix: start script for windows

--- tests/ten_runtime/integration/python/go_app_cythonize/test_case.py
+++ tests/ten_runtime/integration/python/go_app_cythonize/test_case.py
@@ -203,11 +203,20 @@ def test_go_app_cythonize():
 
             my_env["LSAN_OPTIONS"] = f"suppressions={lsan_suppressions_path}"
 
-    server_cmd = os.path.join(base_path, "go_app_cythonize_app/bin/start")
+    if sys.platform == "win32":
+        start_script = os.path.join(app_root_path, "bin", "start.py")
 
-    if not os.path.isfile(server_cmd):
-        print(f"Server command '{server_cmd}' does not exist.")
-        assert False
+        if not os.path.isfile(start_script):
+            print(f"Server command '{start_script}' does not exist.")
+            assert False
+
+        server_cmd = [sys.executable, start_script]
+    else:
+        server_cmd = os.path.join(app_root_path, "bin/start")
+
+        if not os.path.isfile(server_cmd):
+            print(f"Server command '{server_cmd}' does not exist.")
+            assert False
 
     server = subprocess.Popen(
         server_cmd,
